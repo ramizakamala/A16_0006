f0ee8649c1fe93507c1fba3091bdef91b4ba79fa
Ramiza Kamala Tatsuru <mizarakamala@gmail.com>
2025-01-28 05:36:20 +0700

    Tambah DI container untuk AsetRepository dengan konfigurasi Retrofit

---
+++ app/src/main/java/com/example/final_project/di/AsetContainer.kt
@@ -0,0 +1,32 @@
+package com.example.final_project.di
+
+import com.example.final_project.repository.NetworkAsetRepository
+import com.example.final_project.repository.AsetRepository
+import com.example.final_project.service.AsetService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AsetContainer {
+    val asetRepository: AsetRepository
+}
+
+class setContainer : AsetContainer {
+
+    private val baseUrl = "http://10.0.2.2:3000/api/mahasiswa/" //localhost diganti ip kalo run di hp
+
+    private val json = Json { ignoreUnknownKeys = true}
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val asetService: AsetService by lazy {
+        retrofit.create(AsetService::class.java)
+    }
+
+    override val asetRepository : AsetRepository by lazy {
+        NetworkAsetRepository(asetService)
+    }
+}
