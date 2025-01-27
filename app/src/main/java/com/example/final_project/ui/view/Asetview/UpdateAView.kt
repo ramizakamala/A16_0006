c8d9663a04a148c8525d32b56f1223b9d13793bf
Ramiza Kamala Tatsuru <mizarakamala@gmail.com>
2025-01-28 06:19:15 +0700

    Menambahkan fitur update Aset dengan form input.

---
+++ app/src/main/java/com/example/final_project/ui/view/Asetview/UpdateAView.kt
@@ -0,0 +1,141 @@
+package com.example.final_project.ui.view.Asetview
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.final_project.model.Aset
+import com.example.final_project.ui.customwidget.CostumeTopAppBar
+import com.example.final_project.ui.navigation.DestinasiNavigasi
+import com.example.final_project.ui.viewmodel.Asetviewmodel.UpdateAViewModel
+import com.example.final_project.ui.viewmodel.Asetviewmodel.UpdateUiEvent
+import com.example.final_project.ui.viewmodel.Asetviewmodel.UpdateUiState
+import com.example.final_project.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiUpdateA: DestinasiNavigasi {
+    override val route = "update"
+    override val titleRes = "Update Aset"
+    const val aset = "aset"
+    val routeWithArgs = "$route/{$aset}"
+}
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateSetScreen(
+    aset: String,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateAViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    LaunchedEffect(aset) {
+        viewModel.loadAset(aset )
+    }
+
+    val updateUiState = viewModel.UpdateuiState
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiUpdateA.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        UpdateBody(
+            updateUiState = updateUiState,
+            onValueChange = viewModel::updateState,
+            onUpdateClick = {
+                coroutineScope.launch {
+                    viewModel.updateSet()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun UpdateBody(
+    updateUiState: UpdateUiState,
+    onValueChange: (UpdateUiEvent) -> Unit,
+    onUpdateClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(26.dp)
+    ) {
+        UpdateFormInput(
+            updateUiEvent = updateUiState.updateuiEvent,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onUpdateClick,
+            shape = MaterialTheme.shapes.small,
+        ) {
+            Text(text = "Update")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateFormInput(
+    updateUiEvent: UpdateUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (UpdateUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = updateUiEvent.idaset,
+            onValueChange = { onValueChange(updateUiEvent.copy(idaset = it)) },
+            label = { Text(text = "aset") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = updateUiEvent.namaaset,
+            onValueChange = { onValueChange(updateUiEvent.copy(namaaset = it)) },
+            label = { Text(text = "Nama Aset") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = false, // NIM tidak boleh diubah
+            singleLine = true
+        )
+    }
+}
