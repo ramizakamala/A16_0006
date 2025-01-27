6525b2bb4c1a728213833314094bae3c7cbda063
Ramiza Kamala Tatsuru <mizarakamala@gmail.com>
2025-01-28 05:55:20 +0700

    Menambahkan tampilan detail pendapatan dengan fitur hapus dan edit.

---
+++ app/src/main/java/com/example/final_project/ui/view/Pendapatanview/DetailPView.kt
@@ -0,0 +1,254 @@
+package com.example.final_project.ui.view.Pendapatanview
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.final_project.model.Pendapatan
+import com.example.final_project.ui.customwidget.CostumeTopAppBar
+import com.example.final_project.ui.navigation.DestinasiNavigasi
+import com.example.final_project.ui.viewmodel.Pendapatanviewmodel.DetailPViewModel
+import com.example.final_project.ui.viewmodel.Pendapatanviewmodel.DetailUiState
+import com.example.final_project.ui.viewmodel.PenyediaViewModel
+
+object DestinasiDetailP: DestinasiNavigasi {
+    override val route = "detail"
+    override val titleRes = "Detail Pendapatan"
+    const val idpendapatan = "idpendapatan"
+    val routeWithArgs = "$route/{$idpendapatan}"
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah Anda Yakin Ingin Menghapus Data Ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
+
+@Composable
+fun ComponentDetailPend(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+) {
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ){
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+fun ItemDetailPend(
+    modifier: Modifier = Modifier,
+    pendapatan: Pendapatan
+) {
+    Card (
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ){
+            ComponentDetailPend(judul = "id pendapatan", isinya = pendapatan.idpendapatan)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailPend(judul = "id aset", isinya = pendapatan.idaset)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailPend(judul = "id kategori", isinya = pendapatan.idkategori)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailPend(judul = "tanggal transaksi", isinya = pendapatan.tanggaltransaksi)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+
+            ComponentDetailPend(judul = "total", isinya = pendapatan.total)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailPend(judul = "catatan", isinya = pendapatan.catatan)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+        }
+    }
+}
+
+@Composable
+fun BodyDetailPend(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState,
+    onDeleteClick: () -> Unit = { }
+) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+    when (detailUiState) {
+        is DetailUiState.Loading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        is DetailUiState.Success -> {
+            Column(
+                modifier = modifier.fillMaxWidth().padding(16.dp)
+            ) {
+                ItemDetailPend(
+                    pendapatan = detailUiState.pendapatan,
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button(
+                    onClick = { deleteConfirmationRequired = true },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+        is DetailUiState.Error -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data Tidak Ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailPView(
+    idpendapatan: String,
+    modifier: Modifier = Modifier,
+    viewModel: DetailPViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    navigateBack: () -> Unit,
+    onEditClick: (String) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+) {
+
+    LaunchedEffect(idpendapatan) {
+        viewModel.getPendById(idpendapatan)
+    }
+
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetailP.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { onEditClick(idpendapatan) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Pendapatan"
+                )
+            }
+        }
+    ) { innerPadding ->
+        val detailUiState by viewModel.detailUiState.collectAsState()
+
+        BodyDetailPend(
+            modifier = modifier.padding(innerPadding),
+            detailUiState = detailUiState,
+            onDeleteClick = {
+                viewModel.deletepend(idpendapatan)
+                onDeleteClick()
+            }
+        )
+    }
+}
