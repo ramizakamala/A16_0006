a659e9a3dd5c436ea707396a12567d66c1b4e6fd
Ramiza Kamala Tatsuru <mizarakamala@gmail.com>
2025-01-28 05:35:19 +0700

    Tambah DI container untuk KategoriRepository dengan konfigurasi Retrofit

---
+++ app/src/main/java/com/example/final_project/di/KategoriContainer.kt
@@ -0,0 +1,30 @@
+package com.example.final_project.di
+
+import com.example.final_project.repository.KategoriRepository
+import com.example.final_project.repository.NetworkKategoriRepository
+import com.example.final_project.service.KategoriService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface KategoriContainer {
+    val kategoriRepository: KategoriRepository
+}
+
+class KateContainer : KategoriContainer {
+
+    private val baseUrl = "http://10.0.2.2:3000/api/mahasiswa/" //localhost diganti ip kalo run di hp
+
+    private val json = Json { ignoreUnknownKeys = true}
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+    private val kategoriService: KategoriService by lazy {
+        retrofit.create(KategoriService::class.java)
+    }
+    override val kategoriRepository: KategoriRepository by lazy {
+        NetworkKategoriRepository(kategoriService)
+    }
+}
