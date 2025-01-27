18fc80bb18e9f3dac447a70ad2791aa3d6893009
Ramiza Kamala Tatsuru <mizarakamala@gmail.com>
2025-01-28 06:13:52 +0700

    Menambahkan fitur form input untuk tambah Pengeluaran .

---
+++ app/src/main/java/com/example/final_project/ui/view/Pengeluaranview/InsertNView.kt
@@ -0,0 +1,166 @@
+package com.example.final_project.ui.view.Pengeluaranview
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.final_project.ui.customwidget.CostumeTopAppBar
+import com.example.final_project.ui.navigation.DestinasiNavigasi
+import com.example.final_project.ui.viewmodel.Pengeluaranviewmodel.InsertUiEvent
+import com.example.final_project.ui.viewmodel.Pengeluaranviewmodel.InsertUiState
+import com.example.final_project.ui.viewmodel.Pengeluaranviewmodel.InsertViewModel
+import com.example.final_project.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+object DestinasiEntryN: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Insert Pengeluaran"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryPengScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntryN.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){
+            innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onPengeluaranValueChange = viewModel::updateInsertPengState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertPeng()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onPengeluaranValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertUiEvent = insertUiState.InsertUiEvent,
+            onValueChange = onPengeluaranValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = insertUiEvent.idpengeluaran,
+            onValueChange = {onValueChange(insertUiEvent.copy(idpengeluaran = it))},
+            label = { Text(text = "idpengeluaran")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.idaset,
+            onValueChange = {onValueChange(insertUiEvent.copy(idaset = it))},
+            label = { Text(text = "id aset")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.tanggaltransaksi,
+            onValueChange = {onValueChange(insertUiEvent.copy(tanggaltransaksi = it))},
+            label = { Text(text = "tanggal transaksi")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.total,
+            onValueChange = {onValueChange(insertUiEvent.copy(total = it))},
+            label = { Text(text = "total")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.catatan,
+            onValueChange = {onValueChange(insertUiEvent.copy(catatan = it))},
+            label = { Text(text = "catatan")},
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+        if(enabled){
+            Text(
+                text = "Isi Semua Data",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
